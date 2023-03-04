e9d56980ab3aa89b94bca4a6d6be28be7f104fef
jcmisas <jcmisas@hotmail.com>
2023-03-04 09:35:58 -0500

    try-out 1

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,30 +3,48 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = None
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
-        self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+    def __init__(self,grupo="grupo predeterminado", asignaturas=None,estudiantes=None):
 
-    def listadoAsignaturas(self, kwargs):
+        self._grupo = grupo
+
+        Grupo.grado="Grado 12"
+
+        if asignaturas==None:
+
+            self._asignaturas=[]
+        else:
+            self._asignaturas=asignaturas    
+
+        if estudiantes==None:
+            self.listadoAlumnos = []
+            
+        else:
+            self.listadoAlumnos=estudiantes
+
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+    def agregarAlumno(self, alumno, lista=None):
+        if lista==None:
+            lista=[]
+            lista.append(alumno)
+            self.listadoAlumnos = self.listadoAlumnos + lista
+        else:
+            lista.append(alumno)
+            self.listadoAlumnos = self.listadoAlumnos + lista
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return f"Grupo de estudiantes: {self._grupo}"
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
+    def asignarNombre(cls, nombre="Grado 12"):
         cls.grado = nombre
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+    # @ classmethod
+    # def asignarNombre(cls, nombre="Grado 4"):
+    #     cls.grado = nombre
